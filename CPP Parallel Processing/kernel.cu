bb17e1953ebd5e23a20921cd05e5d3b7a3763a43
luizklitzke1 <luiz.klitzke1@gmail.com>
2023-11-13 00:56:26 -0300

    Processamento linear e comaoparação de tempos

--- CPP Parallel Processing/kernel.cu
+++ CPP Parallel Processing/kernel.cu
@@ -15,6 +15,8 @@
 //Limite de threads por bloco = 1024
 #define THREADS_PER_BLOCK 1024
 
+using msTime = std::chrono::duration<double, std::milli>;
+
 int getSPcores(cudaDeviceProp devProp)
 {  
     int cores = 0;
@@ -65,9 +67,9 @@ __global__ void addKernel(const int* vectorA, const int* vectorB, int* sumVector
     sumVector[idxThread] = vectorA[idxThread] + vectorB[idxThread];
 }
 
-cudaError_t addWithCuda(const int* vectorA, const int* vectorB, int* sumVector)
+cudaError_t addWithCuda(const int* vectorA, const int* vectorB, int* sumVector, msTime& processingTime)
 {
-    printf("[CUDA CORES - INÍCIO]\n");
+    printf("\n\n[CUDA CORES - INÍCIO]\n");
     printf("BLOCOS: %d\nTHREADS POR BLOCO: %d\n", BLOCKS, THREADS_PER_BLOCK);
 
     auto clockInicioCuda = std::chrono::high_resolution_clock::now();
@@ -177,12 +179,31 @@ FreeCuda:
     cudaFree(dev_b);
 
     auto clockFimCuda = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, std::milli> msProcessamento = clockFimCuda - clockInicioCuda;
-    printf("Tempo total de processamento com CUDA cores: %fms\n", msProcessamento.count());
+    processingTime = clockFimCuda - clockInicioCuda;
+    printf("Tempo total de processamento com CUDA cores: %fms\n", processingTime.count());
 
     printf("[CUDA CORES - FIM]\n");
 
     return cudaStatus;
+}
+
+void addLinear(const int* vectorA, const int* vectorB, int* sumVector, msTime& processingTime)
+{
+    printf("\n\n[PROCESSAMENTO LINEAR - INÍCIO]\n");
+
+    auto clockInicioLinear = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < THREADS_PER_BLOCK; ++i)
+    {
+        sumVector[i] = vectorA[i] + vectorB[i];
+    }
+
+    auto clockFimLinear = std::chrono::high_resolution_clock::now();
+
+    processingTime = clockFimLinear - clockInicioLinear;
+
+    printf("Tempo total de processamento linear: %fms\n", processingTime.count());
+    printf("[PROCESSAMENTO LINEAR - FIM]\n");
 }
 
 int main()
@@ -208,42 +229,52 @@ int main()
         }
     }
 
-    cudaError_t cudaStatus = addWithCuda(vectorA, vectorB, sumvector);
-    if (cudaStatus != cudaSuccess)
+    msTime CUDAProcessingTime;
+    //Processamento paraleo com CUDA cores
     {
-        printf("Erro ao processar soma em CUDA");
-        return 1;
-    }
-
-    // Validar somas
-    {
-        for (int i = 0; i < THREADS_PER_BLOCK; ++i)
-        {
-            const int valueA   = vectorA  [i];
-            const int valueB   = vectorB  [i];
-            const int sumValue = sumvector[i];
-
-            assert(sumValue == valueA + valueB);
-
-            if (sumValue != valueA + valueB)
-            {
-                printf("[%d][ERRO DE SOMA][Diferença encontrada na soma! - %d + %d != %d\n", i, valueA, valueB, sumValue);
-                return 1;
-            }
-
-            //printf("[%d]%d + %d = %d\n", i, valueA, valueB, sumValue);
-        }
-    }
-
-    // Limpar devices para evitar erros de profiling
-    {
-        cudaStatus = cudaDeviceReset();
+        cudaError_t cudaStatus = addWithCuda(vectorA, vectorB, sumvector, CUDAProcessingTime);
         if (cudaStatus != cudaSuccess)
         {
-            printf("Erro ao executar cudaDeviceReset()");
+            printf("Erro ao processar soma em CUDA");
             return 1;
         }
+
+        // Validar somas
+        {
+            for (int i = 0; i < THREADS_PER_BLOCK; ++i)
+            {
+                const int valueA   = vectorA  [i];
+                const int valueB   = vectorB  [i];
+                const int sumValue = sumvector[i];
+
+                assert(sumValue == valueA + valueB);
+
+                if (sumValue != valueA + valueB)
+                {
+                    printf("[%d][ERRO DE SOMA][Diferença encontrada na soma! - %d + %d != %d\n", i, valueA, valueB, sumValue);
+                    return 1;
+                }
+
+                //printf("[%d]%d + %d = %d\n", i, valueA, valueB, sumValue);
+            }
+        }
+
+        // Limpar devices para evitar erros de profiling
+        {
+            cudaStatus = cudaDeviceReset();
+            if (cudaStatus != cudaSuccess)
+            {
+                printf("Erro ao executar cudaDeviceReset()");
+                return 1;
+            }
+        }
     }
+
+    //Processamento Linear
+    msTime linearProcessingTime;
+    addLinear(vectorA, vectorB, sumvector, linearProcessingTime);
+
+    printf("\n\n[DIF] Diferença entre processamento linear e paralelizado com CUDA cores = %f\n", linearProcessingTime.count() - CUDAProcessingTime.count());
 
     return 0;
 }
